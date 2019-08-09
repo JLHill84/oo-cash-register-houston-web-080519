baa0a7a7172c06bdc144ab1d39ce285eb5f4a61f
Joshua Hill <josh@jlhworkshop.com>
2019-08-09 14:02:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     return @total
   end
   
-  def add_item(title, price, quantity*)
+  def add_item(title, price, quant)
     # @prevTotal = @total
     @total += price
     # @items.push(title)
