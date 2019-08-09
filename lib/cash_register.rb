ada0c12aede37e11b7e9764ee13410a9bc93edcb
Joshua Hill <josh@jlhworkshop.com>
2019-08-09 13:37:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
 
+class CashRegister
+  
+  def initialize (discount = 20)
+    @discount = discount
+    @total = 0
+  end
+  
+  def total
+    @total
+    return @total
+  end
+  
+  def apply_discount
+    @cash_register_with_discount = 20
+  end
+  
+  def add_item(title, price, quant = 1)
+    @total += price
+  end
+  
+  def apply_discount
+    @total = (@total * @discount)
+  end
+  
+  def void_last_transaction(lastItemCost)
+    @total -+ lastItemCost
+  end
+    
+end
+
+cash_register = CashRegister.new
